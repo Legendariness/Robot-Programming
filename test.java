c22c6eb3bc192c1668f284900a4b2e85c446f300
Legendariness <32422307+Legendariness@users.noreply.github.com>
2017-10-04 07:31:59 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Forward", group="Linear OpMode")
+//Disabled
+
+public class Forward extends Linear OpMode {
+
+
+private ElapsedTime runtime = new ElapsedTime();
+  
+ DcMotor leftMotor;
+ DcMotor rightMotor;
+ 
+ double power = 0.5;
+ 
+@Override 
+  public void runOpMode() throws InterruptedException {
+  
+  telemetry.addData("Status", "Initialized");
+  telemetry.update();
+ 
+    leftMotor = hardwareMap.dcMotor.get("Left_Motor");
+    rightMotor = hardwareMap.dcMotor.get("Right_Motor");
+    
+    
+    leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    
+    
+    waitForStart();
+    runtime.reset();
+    
+    
+    
+    leftMotor.setPower(power);
+    rightMotor.setPower(power);
+    
+    sleep(2000);
+    
+    power = 0.0;
+    
+    
+    leftMotor.setPower(power);
+    rightMotor.setPower(power);
+  }
+}
+
 
