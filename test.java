c5e85aea150081f6ba1e199c07e7f6017fe0d011
Legendariness <32422307+Legendariness@users.noreply.github.com>
2017-10-04 09:06:25 -0400

    Update test.java

--- test.java
+++ test.java
@@ -7,10 +7,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="Forward", group="Linear OpMode")
+@Autonomous(name="DriveSquare", group="Linear OpMode")
 //Disabled
 
-public class Forward extends Linear OpMode {
+public class DriveSquare extends Linear OpMode {
 
 
 private ElapsedTime runtime = new ElapsedTime();
@@ -36,18 +36,25 @@ private ElapsedTime runtime = new ElapsedTime();
     waitForStart();
     runtime.reset();
     
-    
+    for (int i = 0; i<4; i++ ) { 
     
     leftMotor.setPower(power);
     rightMotor.setPower(power);
-    
     sleep(2000);
-    
-    power = 0.0;
-    
-    
-    leftMotor.setPower(power);
+      
+    leftMotor.setPower(-power);
     rightMotor.setPower(power);
+    sleep(1000);
+    
+    
+    }
+    
+    leftMotor.setPower(0.0);
+    rightMotor.setPower(0.0);
+    
+    
+    leftMotor.setPower(-gamepad1.left_stick_y);
+    rightMotor.setPower(gamepad1.right_stick_y);
   }
 }
 
